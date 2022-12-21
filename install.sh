c9ded3b5ae21ded1cafdb6de3a78d8a3b4b9ff96
zeroruka <89210318+zeroruka@users.noreply.github.com>
2022-12-22 00:57:19 +0800

    fix bugs

--- install.sh
+++ install.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 
-# Installs the script
 # check if the script is already installed
 if [ -f $HOME/.config/video-wallpapers/wallhelper ]; then
     echo "Wallhelper is already installed"
+    echo "Update/reinstall wallhelper? (y/N)"
+    read answer
+    if [ "$answer" = "y" ]; then
+        echo "Updating..."
+        rm -rf $HOME/.config/video-wallpapers
+        mkdir -p $HOME/.config/video-wallpapers
+        mkdir -p $HOME/.config/video-wallpapers/wallpapers
+        touch $HOME/.config/video-wallpapers/config.ini
+        cp scripts/* $HOME/.config/video-wallpapers/
+        chmod +x $HOME/.config/video-wallpapers/wallhelper
+        chmod +x $HOME/.config/video-wallpapers/setwall
+        echo "Successfully updated!"
+    fi
     exit 0
 fi
 
 # create the config directory
 mkdir -p $HOME/.config/video-wallpapers
-mv scripts/* $HOME/.config/video-wallpapers/
+cp scripts/* $HOME/.config/video-wallpapers/
 chmod +x $HOME/.config/video-wallpapers/wallhelper
 chmod +x $HOME/.config/video-wallpapers/setwall
 
 # prompt the user if they want to install example wallpapers
-echo "Do you want to install example wallpapers? (y/n)"
+echo "Do you want to install example wallpapers? (y/N)"
 read answer
 if [ "$answer" = "y" ]; then
     mkdir -p $HOME/.config/video-wallpapers/wallpapers
-    mv examples/* $HOME/.config/video-wallpapers/wallpapers
+    cp examples/* $HOME/.config/video-wallpapers/wallpapers
     echo "1" >$HOME/.config/video-wallpapers/wallpaperVar
 fi
 
