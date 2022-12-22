2b3cb5b6eba5daa2772b31394098a8a13f464d59
zeroruka <89210318+zeroruka@users.noreply.github.com>
2022-12-22 20:33:19 +0800

    Fix updating scripts

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ if [ -f $HOME/.config/video-wallpapers/wallhelper ]; then
     read answer
     if [ "$answer" = "y" ]; then
         echo "Updating..."
+        mv $HOME/.config/video-wallpapers/wallpapers $HOME/.config/
         rm -rf $HOME/.config/video-wallpapers
         mkdir -p $HOME/.config/video-wallpapers
-        mkdir -p $HOME/.config/video-wallpapers/wallpapers
+        mv $HOME/.config/wallpapers $HOME/.config/video-wallpapers
         touch $HOME/.config/video-wallpapers/config.ini
         cp scripts/* $HOME/.config/video-wallpapers/
-        chmod +x $HOME/.config/video-wallpapers/wallhelper
-        chmod +x $HOME/.config/video-wallpapers/setwall
+        echo "1" >$HOME/.config/video-wallpapers/wallpaperVar
         echo "Successfully updated!"
     fi
     exit 0
@@ -22,8 +22,7 @@ fi
 # create the config directory
 mkdir -p $HOME/.config/video-wallpapers
 cp scripts/* $HOME/.config/video-wallpapers/
-chmod +x $HOME/.config/video-wallpapers/wallhelper
-chmod +x $HOME/.config/video-wallpapers/setwall
+echo "1" >$HOME/.config/video-wallpapers/wallpaperVar
 
 # prompt the user if they want to install example wallpapers
 echo "Do you want to install example wallpapers? (y/N)"
